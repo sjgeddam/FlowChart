b2429fcc6b0bd7152d224c9d73c2580fd66d9b18
Soujanya Geddam <sgeddam@utexas.edu>
2020-04-01 17:36:29 -0500

    transfer to Tracker.storyboard

--- FlowChart-iOSProject/ViewController.swift
+++ FlowChart-iOSProject/ViewController.swift
@@ -8,79 +8,16 @@
 
 import UIKit
 
-protocol newSymptom {
-    func addSymptom(symptom:String)
-}
-
-class ViewController: UIViewController, UIPopoverPresentationControllerDelegate, UITableViewDelegate, UITableViewDataSource, newSymptom {
+class ViewController: UIViewController {
     
-    var symptoms: [String] = []
-    let cellReuseIdentifier = "cell"
-    let cellSpacingHeight: CGFloat = 5
-    var customTableViewCellIdentifier = "symptomCell"
-    var popoverSegueIdentifier = "popoverSegue"
-    var popoverVCIdentifier = "popoverVC"
-    
-    //sections are used to add spacing between cells
-    func numberOfSections(in tableView: UITableView) -> Int {
-        return self.symptoms.count
+    @IBAction func segueButtonPressed(_ sender: Any) {
+        performSegue(withIdentifier: "goToTracker", sender: self)
     }
-    
-    //only one row per section
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 1
-    }
-    
-    //setting the spacing between cells
-    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
-        return cellSpacingHeight
-    }
-    
-    //make the background color show through
-    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
-        let headerView = UIView()
-        headerView.backgroundColor = UIColor.clear
-        return headerView
-    }
-    
-    //create a cell for each table view row
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: customTableViewCellIdentifier, for: indexPath) as! CustomTableViewCell
-        // note that indexPath.section is used rather than indexPath.row
-        cell.symptom.text = self.symptoms[indexPath.section]
-
-        // add border and color
-        cell.backgroundColor = UIColor.orange
-        cell.layer.borderColor = UIColor.black.cgColor
-        cell.layer.borderWidth = 1
-        cell.layer.cornerRadius = 8
-        cell.clipsToBounds = true
-        return cell
-    }
-    
-    //segueing into popoverVC after + button is clicked
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        if (segue.identifier == popoverSegueIdentifier) {
-            let destination = segue.destination as? popoverViewController
-            destination?.delegate = self
-        }
-    }
-
-    @IBOutlet weak var date: UILabel!
-    @IBOutlet weak var symptomsTable: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        symptomsTable.delegate = self
-        symptomsTable.dataSource = self
     }
-    
-    func addSymptom(symptom:String) {
-        symptoms.append(symptom)
-        symptomsTable.reloadData()
-    }
-
 
 }
 
