90eda2c5baddeb4e3b2d309d906c8bfa524ee8ef
Shannon Radey <shannonradey@Shannons-MacBook-Pro.local>
2020-04-15 19:47:38 -0400

    added minimal design elements to popover

---
+++ FlowChart-iOSProject/symptomButton.swift
@@ -0,0 +1,21 @@
+//
+//  symptomButton.swift
+//  FlowChart-iOSProject
+//
+//  Created by Shannon Radey on 4/15/20.
+//  Copyright © 2020 Abby Krishnan. All rights reserved.
+//
+
+import UIKit
+
+class symptomButton: UIButton {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
